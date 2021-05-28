1f275b514af3733d304ac803af4521ca4c313e32
kevinmanwailee <kevin.lee5@ucalgary.ca>
2021-05-28 16:10:21 -0600

    Added L-block, textures, removed spaces/scaled down all prefabs to 0.9

---
+++ 3d_tetris/Assets/calculateScore.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class calculateScore : MonoBehaviour
+{
+    public Text score;
+    private float startScore;
+    private bool end = false;
+    private double totalCapacity = 10*10*40;
+    double s = 0;
+    private int frames = 0;
+
+
+    timer timer = new timer();
+    GameManager gm = new GameManager();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScore = 0;
+        score.text = startScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(end)
+            return;
+
+        int blocks = gm.getBlockCounter();
+        double b = (double)blocks;
+        
+        double time = (double)timer.getTime();
+
+        //print("time: " + time);
+        //print("block count: " + b);
+
+        b = b*40000;
+
+        double capacity = b/totalCapacity;
+        double s =+ (capacity*1000)/time;
+        //print("time: " + time);
+        frames++; 
+        if(frames% 100 == 0){
+            score.text = s.ToString("f0");
+        }
+    }
+}
